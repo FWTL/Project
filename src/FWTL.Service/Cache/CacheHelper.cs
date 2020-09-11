10032e54e5a66ab2f360494e8b34808fd851e4da
Andrzej Golaszewski <andrusza1989@gmail.com>
2020-09-11 22:11:53 +0200

    get chat infos

---
+++ src/FWTL.Service/Cache/CacheHelper.cs
@@ -0,0 +1,12 @@
+﻿using FWTL.Domain.Users;
+using FWTL.TelegramClient.Responses;
+
+namespace FWTL.Domain.Cache
+{
+    public static class CacheHelper
+    {
+        public static string GetDialogsAsync(GetDialogs.Query query) => $"GetDialogsAsync.{query.SessionName}";
+
+        public static string GetInfoAsync(GetDialogs.Query query, Dialog dialog) => $"GetInfoAsync.{query.SessionName}.{dialog.Type}.{dialog.Id}";
+    }
+}
