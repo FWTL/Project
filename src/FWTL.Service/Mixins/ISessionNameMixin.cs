ffe54b2b5c3c51f14a97a1ce861e4e450c1e0767
Andrzej Golaszewski <andrusza1989@gmail.com>
2020-09-11 22:32:05 +0200

    c# 8

---
+++ src/FWTL.Service/Mixins/ISessionNameMixin.cs
@@ -0,0 +1,14 @@
+﻿using FWTL.Common.Extensions;
+using System;
+
+namespace FWTL.Domain.Mixins
+{
+    internal interface ISessionNameMixin
+    {
+        string AccountId { get; }
+
+        Guid UserId { get; }
+
+        string SessionName => UserId.ToSession(AccountId);
+    }
+}
