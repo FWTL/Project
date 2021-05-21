73b57c39ec305b9e611008ade19e1a9583162e10
Andrzej Golaszewski <agolasz@softserveinc.com>
2021-05-21 14:20:57 +0200

    saga

--- src/FWTL.Domain/Accounts/AccountSetup/AccountSetupSaga.cs
+++ src/FWTL.Domain/Accounts/AccountSetup/AccountSetupSaga.cs
@@ -27,55 +27,54 @@ namespace FWTL.Domain.Accounts.AccountSetup
                 });
             });
 
+            Event(() => AccountSetupRestarted, x =>
+            {
+                x.CorrelateById(m => m.Message.AccountId);
+                x.SetSagaFactory(context => new AccountSetupState
+                {
+                    CorrelationId = context.Message.AccountId,
+                    ExpirationTokenId = Guid.NewGuid(),
+                });
+            });
+
             Event(() => SessionCreated, x => x.CorrelateById(m => m.Message.AccountId));
             Event(() => CodeSent, x => x.CorrelateById(m => m.Message.AccountId));
             Event(() => SetupFailed, x => x.CorrelateById(m => m.Message.AccountId));
             Event(() => SessionRemoved, x => x.CorrelateById(m => m.Message.AccountId));
-            Event(() => AccountSetupRestarted, x => x.CorrelateById(m => m.Message.AccountId));
             Event(() => AccountDeleted, x => x.CorrelateById(m => m.Message.AccountId));
 
-            InstanceState(x => x.CurrentState, Initialized, WithSession, WaitForCode, Ready);
+            InstanceState(x => x.CurrentState, Initialized,  Ready);
 
-            Schedule(() => Timout, instance => instance.ExpirationTokenId, s =>
+            Schedule(() => Timeout, instance => instance.ExpirationTokenId, s =>
             {
                 s.Delay = TimeSpan.FromMinutes(10);
             });
 
-            //When account is created publish create session command
-            Initially(When(AccountCreated)
-                .TransitionTo(Initialized)
-                .Publish(x => new CreateSession.Command() { CorrelationId = x.Data.CorrelationId, AccountId = x.Instance.CorrelationId }));
+            WhenAccountIsCreatedCreateSession();
+            WhenSessionIsCreatedSendCode();
+            WhenCodeIsSentWaitForUserInputFor10MinBeforeRemovingSession();
+            WhenAccountIsVerifiedFinalize();
 
-            Initially(When(AccountSetupRestarted)
-                .TransitionTo(Initialized)
-                .Publish(x => new CreateSession.Command() { CorrelationId = x.Data.CorrelationId, AccountId = x.Instance.CorrelationId }));
+            WhenSetupFailsScheduleRemoveSession();
+            WhenAccountSetupRestarted();
 
-            //When session is created publish send code
-            During(Initialized, When(SessionCreated)
-                 .TransitionTo(WithSession)
-                 .Publish(x => new SendCode.Command() { CorrelationId = x.CorrelationId.Value, AccountId = x.Instance.CorrelationId }));
-
-            //When code is sent schedule timeout and wait for verification
-            During(WithSession, When(CodeSent)
-                .Schedule(Timout, context => new RemoveSession.Command() { CorrelationId = context.Data.CorrelationId, AccountId = context.Instance.CorrelationId })
-                .TransitionTo(WaitForCode));
-
-            //When account is verified, unscheduled timeout and mark as ready
-            During(WaitForCode, When(AccountVerified)
-                .Unschedule(Timout)
-                .TransitionTo(Ready).Finalize());
-
-            //When setup fails, schedule remove session command
-            DuringAny(When(SetupFailed)
-                .Schedule(Timout, context => new RemoveSession.Command() { CorrelationId = context.Data.CorrelationId, AccountId = context.Instance.CorrelationId }));
-
-            DuringAny(When(Timout.Received).Publish(x => x.Data));
-
+            
             DuringAny(When(SessionRemoved)
                 .TransitionTo(Initialized));
 
+            DuringAny(When(AccountDeleted).Finalize());
+            DuringAny(When(Timeout.Received).Publish(x => x.Data));
+        }
+
+        private void WhenAccountSetupRestarted()
+        {
+            Initially(When(AccountSetupRestarted)
+                .Unschedule(Timeout)
+                .TransitionTo(Initialized)
+                .Publish(x => new CreateSession.Command() { CorrelationId = x.Data.CorrelationId, AccountId = x.Instance.CorrelationId }));
+
             DuringAny(When(AccountSetupRestarted)
-                .Unschedule(Timout));
+                .Unschedule(Timeout));
 
             During(Initialized, When(AccountSetupRestarted)
                 .Publish(x => new CreateSession.Command() { CorrelationId = x.Data.CorrelationId, AccountId = x.Instance.CorrelationId }));
@@ -86,7 +85,42 @@ namespace FWTL.Domain.Accounts.AccountSetup
             During(WaitForCode, When(AccountSetupRestarted)
                 .Publish(x => new SendCode.Command() { CorrelationId = x.CorrelationId.Value, AccountId = x.Instance.CorrelationId }));
 
-            DuringAny(When(AccountDeleted).Finalize());
+        }
+
+        public void WhenAccountIsCreatedCreateSession()
+        {
+            Initially(When(AccountCreated)
+                .TransitionTo(Initialized)
+                .Publish(x => new CreateSession.Command() { CorrelationId = x.Data.CorrelationId, AccountId = x.Instance.CorrelationId }));
+
+        }
+
+        public void WhenSessionIsCreatedSendCode()
+        {
+            During(Initialized, When(SessionCreated)
+                .TransitionTo(WithSession)
+                .Publish(x => new SendCode.Command() { CorrelationId = x.Data.CorrelationId, AccountId = x.Instance.CorrelationId }));
+
+        }
+
+        public void WhenCodeIsSentWaitForUserInputFor10MinBeforeRemovingSession()
+        {
+            During(WithSession, When(CodeSent)
+                .Schedule(Timeout, context => new RemoveSession.Command() { CorrelationId = context.Data.CorrelationId, AccountId = context.Instance.CorrelationId })
+                .TransitionTo(WaitForCode));
+        }
+
+        public void WhenAccountIsVerifiedFinalize()
+        {
+            During(WaitForCode, When(AccountVerified)
+                .Unschedule(Timeout)
+                .TransitionTo(Ready).Finalize());
+        }
+
+        public void WhenSetupFailsScheduleRemoveSession()
+        {
+            DuringAny(When(SetupFailed)
+                .Schedule(Timeout, context => new RemoveSession.Command() { CorrelationId = context.Data.CorrelationId, AccountId = context.Instance.CorrelationId }));
         }
 
         public Event<AccountCreated> AccountCreated { get; }
@@ -105,6 +139,6 @@ namespace FWTL.Domain.Accounts.AccountSetup
 
         public Event<AccountDeleted> AccountDeleted { get; }
 
-        public Schedule<AccountSetupState, RemoveSession.Command> Timout { get; }
+        public Schedule<AccountSetupState, RemoveSession.Command> Timeout { get; }
     }
 }
